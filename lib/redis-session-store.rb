9996d74f7c190f04515c0c8fd389a31591a75666
zchar <zchar@mycolorway.com>
2014-08-09 16:59:48 +0800

    Update JsonSerializer to support rails inner type, like ActionDispatch::Flash::FlashHash

--- lib/redis-session-store.rb
+++ lib/redis-session-store.rb
@@ -159,12 +159,72 @@ class RedisSessionStore < ActionDispatch::Session::AbstractStore
 
   # Uses built-in JSON library to encode/decode session
   class JsonSerializer
-    def self.load(value)
-      JSON.parse(value, quirks_mode: true)
+    SERIALIZABLE = [String, TrueClass, FalseClass, NilClass, Numeric, Date, Time, Symbol]
+    MARSHAL_INDICATORS = ["\x04", "\004", "\u0004"]
+
+    def self.load(string)
+      object = string.start_with?(*MARSHAL_INDICATORS) ? ::Marshal.load(string) : JSON.parse(string, :symbolize_names => true)
+      _unmarshal(object)
+      # JSON.parse(value, quirks_mode: true)
     end
 
-    def self.dump(value)
-      JSON.generate(value, quirks_mode: true)
+    def self.dump(object)
+      object = _marshal(object)
+      JSON.generate(object)
+      # JSON.generate(value, quirks_mode: true)
+    end
+
+    def _marshal(object)
+      case object
+      when Hash
+        marshal_hash(object)
+      when Array
+        object.each_with_index { |v, i| object[i] = _marshal(v) }
+      when Set
+        _marshal(object.to_a)
+      when String
+        object.encoding == Encoding::ASCII_8BIT ? object.to_json_raw_object : object
+      when *SERIALIZABLE
+        object
+      else
+        if is_flash_hash?(object)
+          object
+        else
+          raise "Connot correctly serialize object: #{object.inspect}"
+        end
+      end
+    end
+
+    def is_flash_hash?(object)
+      defined?(ActionDispatch::Flash::FlashHash) &&
+        object.kind_of?(ActionDispatch::Flash::FlashHash)
+    end
+
+    def marshal_hash(object)
+      object.each { |k,v| object[k] = _marshal(v) }
+    end
+
+    def _unmarshal(object)
+      case object
+      when Hash
+        object.each { |k,v| object[k] = k.to_sym == :flash ? _flash_unmarshal(v) :  _unmarshal(v) }
+      when Array
+        object.each_with_index { |v, i| object[i] = _unmarshal(v) }
+      when String
+        object.start_with?(*MARSHAL_INDICATORS) ? ::Marshal.load(object) : object
+      else
+        object
+      end
+    end
+
+    # Unfortunately rails requires the flash hash to be put into a flash hash object
+    def _flash_unmarshal(value)
+      return value unless defined?(ActionDispatch::Flash::FlashHash)
+      used  = value.delete(:used)
+      flash = ActionDispatch::Flash::FlashHash.new.update(value)
+      # preserve what has been used so that .sweep will remove it
+      flash.instance_variable_set("@used", Set.new(used.map(&:to_sym))) if used
+      return flash
     end
   end
 

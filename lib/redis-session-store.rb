9dbada074308e912b14d5be384058a452c28dfd6
zchar <zchar@mycolorway.com>
2014-08-09 17:25:08 +0800

    change to simple mode, just change flash key

--- lib/redis-session-store.rb
+++ lib/redis-session-store.rb
@@ -162,73 +162,77 @@ class RedisSessionStore < ActionDispatch::Session::AbstractStore
   # See: http://making.change.org/post/52724140959/sharing-session-with-rails-3-2-and-friends
   # Also see: http://blog.qdaxb.me/php%E4%B8%8Eruby%E5%85%B1%E4%BA%ABsession%E5%AE%9E%E7%8E%B0%E5%8D%95%E7%82%B9%E7%99%BB%E5%BD%95/
   class JsonSerializer
-    SERIALIZABLE = [String, TrueClass, FalseClass, NilClass, Numeric, Date, Time, Symbol]
-    MARSHAL_INDICATORS = ["\x04", "\004", "\u0004"]
+    # SERIALIZABLE = [String, TrueClass, FalseClass, NilClass, Numeric, Date, Time, Symbol]
+    # MARSHAL_INDICATORS = ["\x04", "\004", "\u0004"]
 
     def self.load(string)
-      object = string.start_with?(*MARSHAL_INDICATORS) ? ::Marshal.load(string) : JSON.parse(string, :symbolize_names => true)
-      _unmarshal(object)
-      # JSON.parse(value, quirks_mode: true)
+      # object = string.start_with?(*MARSHAL_INDICATORS) ? ::Marshal.load(string) : JSON.parse(string, :symbolize_names => true)
+      # _unmarshal(object)
+      original = JSON.parse(string, quirks_mode: true)
+      if original.has_key?('flash')
+        original['flash'] = ActionDispatch::Flash::FlashHash.new.update(original['flash'])
+      end
+      original
     end
 
     def self.dump(object)
-      object = _marshal(object)
-      JSON.generate(object)
-      # JSON.generate(value, quirks_mode: true)
+      # object = _marshal(object)
+      # JSON.generate(object)
+      JSON.generate(object, quirks_mode: true)
     end
 
-    def _marshal(object)
-      case object
-      when Hash
-        marshal_hash(object)
-      when Array
-        object.each_with_index { |v, i| object[i] = _marshal(v) }
-      when Set
-        _marshal(object.to_a)
-      when String
-        object.encoding == Encoding::ASCII_8BIT ? object.to_json_raw_object : object
-      when *SERIALIZABLE
-        object
-      else
-        if is_flash_hash?(object)
-          object
-        else
-          raise "Connot correctly serialize object: #{object.inspect}"
-        end
-      end
-    end
-
-    def is_flash_hash?(object)
-      defined?(ActionDispatch::Flash::FlashHash) &&
-        object.kind_of?(ActionDispatch::Flash::FlashHash)
-    end
-
-    def marshal_hash(object)
-      object.each { |k,v| object[k] = _marshal(v) }
-    end
-
-    def _unmarshal(object)
-      case object
-      when Hash
-        object.each { |k,v| object[k] = k.to_sym == :flash ? _flash_unmarshal(v) :  _unmarshal(v) }
-      when Array
-        object.each_with_index { |v, i| object[i] = _unmarshal(v) }
-      when String
-        object.start_with?(*MARSHAL_INDICATORS) ? ::Marshal.load(object) : object
-      else
-        object
-      end
-    end
-
-    # Unfortunately rails requires the flash hash to be put into a flash hash object
-    def _flash_unmarshal(value)
-      return value unless defined?(ActionDispatch::Flash::FlashHash)
-      used  = value.delete(:used)
-      flash = ActionDispatch::Flash::FlashHash.new.update(value)
-      # preserve what has been used so that .sweep will remove it
-      flash.instance_variable_set("@used", Set.new(used.map(&:to_sym))) if used
-      return flash
-    end
+    # def _marshal(object)
+    #   case object
+    #   when Hash
+    #     marshal_hash(object)
+    #   when Array
+    #     object.each_with_index { |v, i| object[i] = _marshal(v) }
+    #   when Set
+    #     _marshal(object.to_a)
+    #   when String
+    #     object.encoding == Encoding::ASCII_8BIT ? object.to_json_raw_object : object
+    #   when *SERIALIZABLE
+    #     object
+    #   else
+    #     if is_flash_hash?(object)
+    #       object
+    #     else
+    #       raise "Connot correctly serialize object: #{object.inspect}"
+    #     end
+    #   end
+    # end
+    #
+    # def is_flash_hash?(object)
+    #   defined?(ActionDispatch::Flash::FlashHash) &&
+    #     object.kind_of?(ActionDispatch::Flash::FlashHash)
+    # end
+    #
+    # def marshal_hash(object)
+    #   object.each { |k,v| object[k] = _marshal(v) }
+    # end
+    #
+    # def _unmarshal(object)
+    #   case object
+    #   when Hash
+    #     object.each { |k,v| object[k] = k.to_sym == :flash ? _flash_unmarshal(v) :  _unmarshal(v) }
+    #   when Array
+    #     object.each_with_index { |v, i| object[i] = _unmarshal(v) }
+    #   when String
+    #     object.start_with?(*MARSHAL_INDICATORS) ? ::Marshal.load(object) : object
+    #   else
+    #     object
+    #   end
+    # end
+    #
+    # # Unfortunately rails requires the flash hash to be put into a flash hash object
+    # def _flash_unmarshal(value)
+    #   return value unless defined?(ActionDispatch::Flash::FlashHash)
+    #   used  = value.delete(:used)
+    #   flash = ActionDispatch::Flash::FlashHash.new.update(value)
+    #   # preserve what has been used so that .sweep will remove it
+    #   flash.instance_variable_set("@used", Set.new(used.map(&:to_sym))) if used
+    #   return flash
+    # end
   end
 
   # Transparently migrates existing session values from Marshal to JSON
